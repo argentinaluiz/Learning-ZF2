fd63754b84d974ff0c700ab94077e848f7912179
Candido Souza <candido_souzza@hotmail.com>
2015-07-28 22:57:23 -0300

    Configurando  ServiceConfig

--- 01-ZF2-Iniciante/module/Bookstore/Module.php
+++ 01-ZF2-Iniciante/module/Bookstore/Module.php
@@ -2,6 +2,8 @@
 
 namespace Bookstore;
 
+use Bookstore\Model\CategoryTable;
+use Bookstore\Services\CategoryService;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
@@ -21,5 +23,20 @@ class Module
                 ),
             ),
         );
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                'Bookstore\Services\CategoryService' => function ($service) {
+                    $dbAdapter = $service->get('Zend\Db\Adapter\Adapter');
+                    $categoryTable = new CategoryTable($dbAdapter);
+                    $categoryService = new CategoryService($categoryTable);
+
+                    return $categoryService;
+                }
+            ]
+        ];
     }
 }

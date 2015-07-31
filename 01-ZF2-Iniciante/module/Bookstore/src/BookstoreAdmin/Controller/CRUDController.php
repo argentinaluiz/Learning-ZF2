57f5d7dcd0736ba5fdd86e182d407f50394b3d11
Candido Souza <candido_souzza@hotmail.com>
2015-07-30 22:52:50 -0300

    Abstraindo o CRUD

---
+++ 01-ZF2-Iniciante/module/Bookstore/src/BookstoreAdmin/Controller/CRUDController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace BookstoreAdmin\Controller;
+
+use Doctrine\ORM\EntityManager;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+use Zend\Paginator\Paginator;
+use Zend\Paginator\Adapter\ArrayAdapter;
+
+/**
+ * Class CRUDController
+ * @package BookstoreAdmin\Controller
+ */
+abstract class CRUDController extends AbstractActionController
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+    protected $service;
+    protected $entity;
+    protected $form;
+    protected $route;
+    protected $controller;
+
+    /**
+     * Categories list
+     *
+     * @return ViewModel
+     */
+    public function IndexAction()
+    {
+        $listCategory = $this->getEm()
+            ->getRepository($this->entity)
+            ->findAll();
+
+        $page = $this->params()->fromRoute('page');
+        $pagination = new Paginator(new ArrayAdapter($listCategory));
+        $pagination->setCurrentPageNumber($page);
+        $pagination->setDefaultItemCountPerPage(10);
+
+        return new ViewModel(['data' => $pagination, 'page' => $page]);
+    }
+
+    /**
+     * Insert new category
+     *
+     * @return \Zend\Http\Response|ViewModel
+     */
+    public function newAction()
+    {
+        $form = new $this->form();
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+
+                $service = $this->getServiceLocator()->get($this->service);
+                $service->insert($request->getPost()->toArray());
+
+                return $this->redirect()->toRoute($this->route, ['controller' => $this->controller]);
+            }
+        }
+
+        return new ViewModel(['form' => $form]);
+    }
+
+    public function EditAction()
+    {
+        $form = new $this->form();
+        $request = $this->getRequest();
+
+        $repository = $this->getEm()->getRepository($this->entity);
+        $entity = $repository->find($this->params()->fromRoute('id', 0));
+
+        if ($this->params()->fromRoute('id', 0))
+            $form->setData($entity->toArray());
+
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $service = $this->getServiceLocator()->get($this->service);
+                $service->update($request->getPost()->toArray());
+
+                return $this->redirect()->toRoute($this->route, ['controller' => $this->controller]);
+            }
+        }
+
+        return new ViewModel(['form' => $form]);
+    }
+
+    public function deleteAction()
+    {
+        $service = $this->getServiceLocator()->get($this->service);
+
+        if ($service->delete($this->params()->fromRoute('id', 0)))
+            return $this->redirect()->toRoute($this->route, ['controller' => $this->controller]);
+    }
+
+    /**
+     * @return EntityManager
+     */
+    private function getEm()
+    {
+        if (null === $this->em)
+            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+
+        return $this->em;
+    }
+}
--- 01-ZF2-Iniciante/module/Bookstore/src/BookstoreAdmin/Controller/CategoriesController.php
+++ 01-ZF2-Iniciante/module/Bookstore/src/BookstoreAdmin/Controller/CategoriesController.php
@@ -2,108 +2,18 @@
 
 namespace BookstoreAdmin\Controller;
 
-use Doctrine\ORM\EntityManager;
-
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
-
-use Zend\Paginator\Paginator;
-use Zend\Paginator\Adapter\ArrayAdapter;
-
-use BookstoreAdmin\Form\Category as FormCategory;
-
-class CategoriesController extends AbstractActionController
+/**
+ * Class CategoriesController
+ * @package BookstoreAdmin\Controller
+ */
+class CategoriesController extends CRUDController
 {
-    /**
-     * @var EntityManager
-     */
-    private $em;
-
-    /**
-     * Categories list
-     *
-     * @return ViewModel
-     */
-    public function IndexAction()
+    public function __construct()
     {
-        $listCategory = $this->getEm()
-            ->getRepository('Bookstore\Entity\Category')
-            ->findAll();
-
-        $page = $this->params()->fromRoute('page');
-        $pagination = new Paginator(new ArrayAdapter($listCategory));
-        $pagination->setCurrentPageNumber($page);
-        $pagination->setDefaultItemCountPerPage(10);
-
-        return new ViewModel(['data' => $pagination, 'page' => $page]);
-    }
-
-    /**
-     * Insert new category
-     *
-     * @return \Zend\Http\Response|ViewModel
-     */
-    public function newAction()
-    {
-        $form = new FormCategory();
-        $request = $this->getRequest();
-
-        if ($request->isPost()) {
-            $form->setData($request->getPost());
-
-            if ($form->isValid()) {
-
-                $service = $this->getServiceLocator()->get('Bookstore\Service\Category');
-                $service->insert($request->getPost()->toArray());
-
-                return $this->redirect()->toRoute('home-admin', ['controller' => 'categories']);
-            }
-        }
-
-        return new ViewModel(['form' => $form]);
-    }
-
-    public function EditAction()
-    {
-        $form = new FormCategory();
-        $request = $this->getRequest();
-
-        $repository = $this->getEm()->getRepository('Bookstore\Entity\Category');
-        $entity = $repository->find($this->params()->fromRoute('id', 0));
-
-        if ($this->params()->fromRoute('id', 0))
-            $form->setData($entity->toArray());
-
-        if ($request->isPost()) {
-            $form->setData($request->getPost());
-
-            if ($form->isValid()) {
-                $service = $this->getServiceLocator()->get('Bookstore\Service\Category');
-                $service->update($request->getPost()->toArray());
-
-                return $this->redirect()->toRoute('home-admin', ['controller' => 'categories']);
-            }
-        }
-
-        return new ViewModel(['form' => $form]);
-    }
-
-    public function deleteAction()
-    {
-        $service = $this->getServiceLocator()->get('Bookstore\Service\Category');
-
-        if ($service->delete($this->params()->fromRoute('id', 0)))
-            return $this->redirect()->toRoute('home-admin', ['controller' => 'categories']);
-    }
-
-    /**
-     * @return EntityManager
-     */
-    private function getEm()
-    {
-        if (null === $this->em)
-            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-
-        return $this->em;
+        $this->entity = 'Bookstore\Entity\Category';
+        $this->form = 'BookstoreAdmin\Form\Category';
+        $this->service = 'Bookstore\Service\Category';
+        $this->controller = 'categories';
+        $this->route = 'home-admin';
     }
 }

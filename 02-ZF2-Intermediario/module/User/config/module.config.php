91984cc542a1eb22539e37452a11431c6c3dd98a
Candido Souza <candido_souzza@hotmail.com>
2015-08-04 00:02:10 -0300

    Configurações iniciais do módulo User

--- 02-ZF2-Intermediario/module/User/config/module.config.php
+++ 02-ZF2-Intermediario/module/User/config/module.config.php
@@ -1,4 +1,60 @@
 <?php
 namespace User;
 
-return [];
+return [
+    'router' => [
+        'routes' => [
+            'user-register' => [
+                'type' => 'Literal',
+                'options' => [
+                    'route' => '/register',
+                    '__NAMESPACE__' => 'User/Controller',
+                    'controller' => 'Index',
+                    'action' => 'register'
+                ]
+            ]
+        ]
+    ],
+    'controllers' => [
+        'invokables' => [
+            'User\Controller\Index' => 'User\Controller\IndexController',
+            ''
+        ]
+    ],
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => [
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+    //    MAPPING DOCTRINE WITH YAML
+    'doctrine' => [
+        'driver' => [
+            '_driver' => [
+                'class' => 'Doctrine\ORM\Mapping\Driver\YamlDriver',
+                'cache' => 'array',
+                'extension' => '.orm.yml',
+                'paths' => [
+                    __DIR__ . '/../src/' . __NAMESPACE__ . '/Resources/config/doctrine/'
+                ]
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    __NAMESPACE__ . '\Entity' => '_driver',
+                ],
+            ],
+        ],
+    ],
+    'data-fixture' => [
+        'User_fixture' => __DIR__ . '/../src/User/Fixture',
+    ],
+];

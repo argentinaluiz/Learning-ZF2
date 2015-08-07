8c4c27eb741b35ae39393c4acd1ce0c6266a18d8
Candido Souza <candido_souzza@hotmail.com>
2015-08-06 21:53:53 -0300

    Implementando o UserRepositori para verificação de senha

--- 02-ZF2-Intermediario/module/User/src/User/Entity/UserRepository.php
+++ 02-ZF2-Intermediario/module/User/src/User/Entity/UserRepository.php
@@ -6,5 +6,18 @@ use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
+    public function findByEmailAndPassword($email, $password)
+    {
+        $user = $this->findOneByEmail($email);
 
+        if ($user) {
+            $hashPassword = $user->encryptPassword($password);
+
+            if ($hashPassword == $user->getPassword())
+                return $user;
+            return false;
+        }
+
+        return false;
+    }
 }

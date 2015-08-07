4185f9eecf63bf7d6bc3d40bf27448a50bd3bac4
Candido Souza <candido_souzza@hotmail.com>
2015-08-06 22:35:18 -0300

    Configurando o AuthController

--- 02-ZF2-Intermediario/module/User/src/User/Controller/AuthController.php
+++ 02-ZF2-Intermediario/module/User/src/User/Controller/AuthController.php
@@ -1,15 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: candidosouza
- * Date: 06/08/15
- * Time: 22:19
- */
 
 namespace User\Controller;
 
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
 
-class AuthController
+use Zend\Authentication\AuthenticationService;
+use Zend\Authentication\Storage\Session as SessionStorage;
+
+use User\Form\Login as FormLogin;
+
+class AuthController extends AbstractActionController
 {
+    public function indexAction()
+    {
+        $form = new FormLogin();
+        $error = false;
+        $request = $this->getRequest();
 
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $data = $request->getPost()->toArray();
+
+                $auth = new AuthenticationService();
+
+                $sessionStorage = new SessionStorage('User');
+                $auth->setStorage($sessionStorage);
+
+                $authAdapter = $this->getServiceLocator()->get('User\Auth\Adapter');
+                $authAdapter->setUsername($data['email'])
+                    ->setPassword($data['password']);
+
+                $result = $auth->authenticate($authAdapter);
+
+                if ($result->isValid()) {
+                    $sessionStorage->write($auth->getIdentity()['user'], null);
+                    return $this->redirect()->toRoute('user-admin/default', ['controller' => 'user  ']);
+                } else
+                    $error = true;
+            }
+        }
+
+        return new ViewModel(['form' => $form, 'error' => $error]);
+    }
 }

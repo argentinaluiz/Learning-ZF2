512704dbe56f7d6761931bcc85ba4e5fb4ed8acd
Candido Souza <candido_souzza@hotmail.com>
2015-08-10 10:36:45 -0300

    Criando o BaseAbstractService e RoleService

---
+++ 02-ZF2-Intermediario/module/Base/src/Base/Service/AbstractService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Base\Service;
+
+use Doctrine\ORM\EntityManager;
+use Zend\Stdlib\Hydrator;
+
+abstract class AbstractService
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    protected $entity;
+
+    /**
+     * Construct
+     *
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Insert
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public function insert(array $data)
+    {
+        $entity = new $this->entity($data);
+
+        $this->em->persist($entity);
+        $this->em->flush();
+        return $entity;
+    }
+
+    /**
+     * Update
+     *
+     * @param array $data
+     * @return bool|\Doctrine\Common\Proxy\Proxy|null|object
+     */
+    public function update(array $data)
+    {
+        $entity = $this->em->getReference($this->entity, $data['id']);
+        (new Hydrator\ClassMethods())->hydrate($data, $entity);
+
+        $this->em->persist($entity);
+        $this->em->flush();
+
+        return $entity;
+    }
+
+    /**
+     * Delete
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        $entity = $this->em->getReference($this->entity, $id);
+
+        if ($entity) {
+            $this->em->remove($entity);
+            $this->em->flush();
+
+            return $id;
+        }
+    }
+}

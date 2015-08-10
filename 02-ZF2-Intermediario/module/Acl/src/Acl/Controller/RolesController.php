2953690bbadcbd5a8cd0e598dd81230deb8f38f0
Candido Souza <candido_souzza@hotmail.com>
2015-08-10 14:39:17 -0300

    Criando Edição

--- 02-ZF2-Intermediario/module/Acl/src/Acl/Controller/RolesController.php
+++ 02-ZF2-Intermediario/module/Acl/src/Acl/Controller/RolesController.php
@@ -60,4 +60,34 @@ class RolesController extends CrudController
 
         return new ViewModel(['form' => $form]);
     }
+
+    /**
+     * Edit
+     *
+     * @return \Zend\Http\Response|ViewModel
+     */
+    public function EditAction()
+    {
+        $form = $this->getServiceLocator()->get('Acl\Form\Role');
+        $request = $this->getRequest();
+
+        $repository = $this->getEm()->getRepository($this->entity);
+        $entity = $repository->find($this->params()->fromRoute('id', 0));
+
+        if ($this->params()->fromRoute('id', 0))
+            $form->setData($entity->toArray());
+
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $service = $this->getServiceLocator()->get($this->service);
+                $service->update($request->getPost()->toArray());
+
+                return $this->redirect()->toRoute($this->route, ['controller' => $this->controller]);
+            }
+        }
+
+        return new ViewModel(['form' => $form]);
+    }
 }

29fdad4b1d79a972e5a6a4cd571535c62fb58d88
Candido Souza <candido_souzza@hotmail.com>
2015-08-11 11:57:15 -0300

    Listando usuários pelo id e criando usuário APIRest

--- 02-ZF2-Intermediario/module/UserRest/src/UserRest/Controller/UserRestController.php
+++ 02-ZF2-Intermediario/module/UserRest/src/UserRest/Controller/UserRestController.php
@@ -9,25 +9,46 @@ use Zend\View\Model\JsonModel;
 class UserRestController extends AbstractRestfulController
 {
 
+    /**
+     * List User API Rest
+     *
+     * @return JsonModel
+     */
     public function getList()
     {
         $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-
         $repository = $em->getRepository('User\Entity\User');
-
         $data = $repository->findArray();
 
         return new JsonModel(['data' => $data]);
     }
 
+    /**
+     * List User id API Rest
+     *
+     * @param mixed $id
+     * @return JsonModel
+     */
     public function get($id)
     {
+        $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        $repository = $em->getRepository('User\Entity\User');
+        $data = $repository->find($id)->toArray();
 
+        return new JsonModel(['data' => $data]);
     }
 
     public function create($data)
     {
+        $userService = $this->getServiceLocator()->get('User\Service\User');
 
+        if ($data) {
+            $user = $userService->insert($data);
+            if ($user)
+                return new JsonModel(['data' => ['id' => $user->getId(), 'success' => true]]);
+            return new JsonModel(['data' => ['success' => false]]);
+        }
+        return new JsonModel(['data' => ['success' => false]]);
     }
 
     public function update($id, $data)

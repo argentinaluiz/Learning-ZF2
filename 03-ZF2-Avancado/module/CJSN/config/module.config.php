91ba29fb54a875fd788ef9703b99945894a90cda
Candido Souza <candido_souzza@hotmail.com>
2015-08-11 15:50:28 -0300

    Criando o módulo CJSN

---
+++ 03-ZF2-Avancado/module/CJSN/config/module.config.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    'router' => [
+        'routes' => [
+            'home' => [
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => [
+                    'route'    => '/cjsn/',
+                    'defaults' => [
+                        'controller' => 'CJSN\Controller\Index',
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'service_manager' => [
+        'abstract_factories' => [
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ],
+        'factories' => [
+            'translator' => 'Zend\Mvc\Service\TranslatorServiceFactory',
+        ],
+    ],
+    'translator' => [
+        'locale' => 'en_US',
+        'translation_file_patterns' => [
+            [
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ],
+        ],
+    ],
+    'controllers' => [
+        'invokables' => [
+            'CJSN\Controller\Index' => 'CJSN\Controller\IndexController'
+        ],
+    ],
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'cjsn/index/index' => __DIR__ . '/../view/cjsn/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+    // Placeholder for console routes
+    'console' => [
+        'router' => [
+            'routes' => [
+            ],
+        ],
+    ],
+];
